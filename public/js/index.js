35c29f97c349d5eff1b1712c7cd1bf1ad0102ff8
unknown <yosefbz@gmail.com>
2018-10-27 20:40:40 +0300

    new newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('connected to server');
+
+  socket.emit('createMessage', {
+    from: 't@t',
+    text: 'Hey'
+  });
+});
+
+
+socket.on('newMessage', function (message) {
+  console.log('New message', message);
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 't@t',
+    text: 'Hey',
+    creatAt: 123
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('Creat Message', newMessage);
+  });
+
   socket.on('disconnect', () => {
     console.log('Disconnected from server');
   });
